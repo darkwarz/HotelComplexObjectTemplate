029c4bc9faa53a996a9593f1d9907a46b6d375ba
Jabier Espinal <jabierespinal@gmail.com>
2017-11-20 17:52:17 +0000

    dynamic button

--- scripts/index.js
+++ scripts/index.js
@@ -19,3 +19,29 @@ var hotel = {
     name: "CareerDevs Innt"
 }
 
+//console.log(document.getElementById("label").nodeName);
+
+for (var i = 0; i < hotel.rooms.length; i++){
+    console.log("start");
+    let radioBtn = document.createElement("INPUT");
+    let radioLbl = document.createElement("LABEL");
+    radioBtn.setAttribute("type", "radio");
+    radioBtn.setAttribute("name", "rooms");
+    radioBtn.setAttribute("value", i);
+    radioBtn.setAttribute("id", "room" + i);
+    radioLbl.innerHTML = hotel.rooms[i].name;
+    console.log("stop");
+
+    document.getElementById("radialSection").appendChild(radioBtn);    
+    document.getElementById("radialSection").appendChild(radioLbl);
+    }
+
+
+//    html dom setAttribute() Method
+    //create input tag 
+document.createElement()
+
+//console.log(hotel.rooms[0].name);
+//displays rooms as radio options
+// on form submission confirm radio was selected
+//and checkbox checked
